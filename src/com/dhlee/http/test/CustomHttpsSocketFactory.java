e4989f5228a7e3fbf11d56e45310814640d3f4f7
rxnzero <rxnzero#gmail.com>
2017-08-11 09:06:26 +0900

    edit

---
+++ src/com/dhlee/http/test/CustomHttpsSocketFactory.java
@@ -0,0 +1,56 @@
+package com.dhlee.http.test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import javax.net.ssl.SSLSocket;
+
+import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
+
+public class CustomHttpsSocketFactory implements SecureProtocolSocketFactory
+{
+
+   private final SecureProtocolSocketFactory base;
+
+   public CustomHttpsSocketFactory(ProtocolSocketFactory base)
+   {
+      if(base == null || !(base instanceof SecureProtocolSocketFactory)) throw new IllegalArgumentException();
+      this.base = (SecureProtocolSocketFactory) base;
+   }
+
+   private Socket acceptOnlyTLS12(Socket socket)
+   {
+      if(!(socket instanceof SSLSocket)) return socket;
+      SSLSocket sslSocket = (SSLSocket) socket;
+      sslSocket.setEnabledProtocols(new String[]{"TLSv1.2" });
+      
+      for(int i=0; i< sslSocket.getEnabledProtocols().length; i++)
+      System.out.println("EnabledProtocols : " + i + " - " + sslSocket.getEnabledProtocols()[i]);
+      
+      return sslSocket;
+   }
+
+   public Socket createSocket(String host, int port) throws IOException
+   {
+      return acceptOnlyTLS12(base.createSocket(host, port));
+   }
+
+   public Socket createSocket(String host, int port, InetAddress localAddress, int localPort) throws IOException
+   {
+      return acceptOnlyTLS12(base.createSocket(host, port, localAddress, localPort));
+   }
+
+   public Socket createSocket(String host, int port, InetAddress localAddress, int localPort, HttpConnectionParams params) throws IOException
+   {
+      return acceptOnlyTLS12(base.createSocket(host, port, localAddress, localPort, params));
+   }
+
+   public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException
+   {
+      return acceptOnlyTLS12(base.createSocket(socket, host, port, autoClose));
+   }
+
+}
---
+++ src/com/dhlee/http/test/TestHttps.java
@@ -0,0 +1,44 @@
+package com.dhlee.http.test;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.protocol.Protocol;
+
+public class TestHttps {
+	public static void main(String[] args) throws HttpException, IOException {
+
+		System.out.println(System.getProperty("java.vendor"));
+		 System.out.println(System.getProperty("java.vendor.url"));
+		 System.out.println(System.getProperty("java.version"));
+		HttpClient httpclient = new HttpClient();
+
+		//		Protocol myhttps = new Protocol("https", new CustomHttpsSocketFactory(new EasySSLProtocolSocketFactory()), 8443);
+//		Protocol myhttps = new Protocol("https", new EasySSLProtocolSocketFactory(), 8443);
+//		httpclient.getHostConfiguration().setHost("localhost", 8443, myhttps);
+//		GetMethod httpget = new GetMethod("/");
+		
+		Protocol myhttps = new Protocol("https", new EasySSLProtocolSocketFactory(), 443);
+		httpclient.getHostConfiguration().setHost("cryptoreport.websecurity.symantec.com", 443, myhttps);
+		GetMethod httpget = new GetMethod("/checker/");
+
+//		 Protocol.registerProtocol("https", 
+//		new Protocol("https", new EasySSLProtocolSocketFactory(), 443));
+//		HttpClient httpclient = new HttpClient();
+//		GetMethod httpget = new GetMethod("https://cryptoreport.websecurity.symantec.com/checker/");
+
+		try {
+		  httpclient.executeMethod(httpget);
+		  System.out.println(httpget.getStatusLine());
+		} finally {
+			httpget.releaseConnection();
+		}
+		
+		
+		
+
+				
+	}
+}
